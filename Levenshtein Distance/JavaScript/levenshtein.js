ab7be93c77b77f488eaff1d7562d2e0e8b9aa20a
Jac21 <jcantu521@gmail.com>
2016-06-10 15:48:15 -0500

    Updates to JavaScript Levenshtein implementation

--- Levenshtein Distance/JavaScript/levenshtein.js
+++ Levenshtein Distance/JavaScript/levenshtein.js
@@ -23,5 +23,52 @@ function levenshtein(stringOne, stringTwo) {
 }
 
 var output = document.getElementById('distance');
-var inputA = "Jeremy", inputB = "Germany";
-output.innerText = ("Levenshtein distance for " + inputA + " and " + inputB + ": " + levenshtein("Jeremy", "Germany"));
+var inputA = "String", inputB = "Strung";
+output.innerText = ("Levenshtein distance for " + inputA + " and " + inputB + ": " + levenshtein(inputA, inputB));
+
+// test method
+function findLeastEditDistance(inputString, stringArray) {
+	// output string is set as the string with the smallest edit distance,
+	// minimum number is used to check
+	var outputString = "";
+	var minimumNumber = Number.POSITIVE_INFINITY;
+
+	// iterate through input array, find distance between inputString and string in array
+	stringArray.forEach(function(arrayString) {
+		var distance = levenshtein(inputString.toLowerCase(), arrayString.toLowerCase());
+
+		// check if current distance is smaller than the minimum, set minimum and outputString if so
+		if (distance < minimumNumber) {
+			minimumNumber = distance
+			outputString = arrayString
+		}
+	});
+
+	return outputString;
+}
+
+var brandArray = ["Inspiron", "XPS", "AlienWare", "Latitude", "Precision", "ChromeBook"];
+var brand = " inspeeron";
+
+var brandOutput = document.getElementById('brand');
+brandOutput.innerText = ("Brand returned: " + (findLeastEditDistance(brand, brandArray)));
+
+// tests
+[ ['', '', 0],
+  ['yo', '', 2],
+  ['', 'yo', 2],
+  ['yo', 'yo', 0],
+  ['tier', 'tor', 2],
+  ['saturday', 'sunday', 3],
+  ['mist', 'dist', 1],
+  ['tier', 'tor', 2],
+  ['kitten', 'sitting', 3],
+  ['stop', 'tops', 2],
+  ['rosettacode', 'raisethysword', 8],
+  ['mississippi', 'swiss miss', 8]
+].forEach(function(v) {
+  var a = v[0], b = v[1], t = v[2], d = levenshtein(a, b);
+  if (d !== t) {
+    console.log('levenstein("' + a + '","' + b + '") was ' + d + ' should be ' + t);
+  }
+});
